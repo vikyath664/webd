a6d6ea0a12b564accab73a30eeb57a73d7791fa7
vikyath664 <komatire@iitg.ac.in>
2020-07-28 12:11:52 +0530

    timepass

--- courses/urls.py
+++ courses/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.timepass, name='timepass'),
     path('postlogin/', views.postloginin, name='postlogin'),
     path('postsignup/', views.postsignup, name='postsignup'),
     url(r'^logout/',views.logout,name="log")
